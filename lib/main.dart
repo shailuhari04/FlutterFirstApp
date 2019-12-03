1c9cba83d9f663d7c88dfcdd1fbcb0416526a20c
fitShailu <57788604+fitshailendra@users.noreply.github.com>
2019-12-03 15:56:22 +0530

    create StateLessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,37 @@ import 'dart:ui' as prefix0;
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Title App"),
-      centerTitle: true,
-      backgroundColor: Colors.red[500],
-    ),
-    body: Center(
-      child: Text(
+  home: TestStateLessWidget()
+));
+
+
+class TestStateLessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title App"),
+        centerTitle: true,
+        backgroundColor: Colors.red[500],
+      ),
+      body: Center(
+        child: Text(
           "Hello Shailendra",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.grey[500],
-          fontFamily: 'RobotoSlab'
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[500],
+              fontFamily: 'RobotoSlab'
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text("Click"),
-      backgroundColor: Colors.red[500],
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+        backgroundColor: Colors.red[500],
+      ),
+    );
+  }
+}
+
