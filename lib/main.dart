1cfabc677fc53ba28b246f061124a5b95f4cb6e6
fitShailu <57788604+fitshailendra@users.noreply.github.com>
2019-12-04 23:06:45 +0530

    Use Of Expanded Widget which is like flex box according to rest of space it will take (calculate each Expanded widget and divide equal size volume

--- lib/main.dart
+++ lib/main.dart
@@ -13,45 +13,30 @@ class TestStateLessWidget extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[500],
       ),
-      body: Column(
+      body: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.blue,
-                child: Text("row item 1"),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                color: Colors.red,
-                child: Text("row item 2"),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.amber,
-                child: Text("row item 3"),
-              )
-            ],
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.blue,
+              child: Text("row item 1"),
+            ),
           ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            color: Colors.amber,
-            child: Text('one'),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.red,
+              child: Text("row item 2"),
+            ),
           ),
-          Container(
-            padding: EdgeInsets.all(30.0),
-            color: Colors.red,
-            child: Text('two'),
-          ),
-          Container(
-            padding: EdgeInsets.all(40.0),
-            color: Colors.blue,
-            child: Text('three'),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.amber,
+              child: Text("row item 3"),
+            ),
           )
         ],
       ),
