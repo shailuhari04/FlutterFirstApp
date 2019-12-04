4278847184d7d4d35caceb6337f66f746006889f
fitShailu <57788604+fitshailendra@users.noreply.github.com>
2019-12-04 22:54:35 +0530

    Use Of Column And Column both

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'dart:ui' as prefix0;
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: TestStateLessWidget()
-));
-
+void main() => runApp(MaterialApp(home: TestStateLessWidget()));
 
 class TestStateLessWidget extends StatelessWidget {
   @override
@@ -17,9 +14,30 @@ class TestStateLessWidget extends StatelessWidget {
         backgroundColor: Colors.red[500],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center ,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.blue,
+                child: Text("row item 1"),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                color: Colors.red,
+                child: Text("row item 2"),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.amber,
+                child: Text("row item 3"),
+              )
+            ],
+          ),
           Container(
             padding: EdgeInsets.all(20.0),
             color: Colors.amber,
@@ -37,7 +55,6 @@ class TestStateLessWidget extends StatelessWidget {
           )
         ],
       ),
-
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text("Click"),
@@ -46,4 +63,3 @@ class TestStateLessWidget extends StatelessWidget {
     );
   }
 }
-
